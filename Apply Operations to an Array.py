5b541f332b7aaac6ffe4a46ed95b8bf5ff5a7d9b
zekariyas716-ux <zekariyas716@gmail.com>
2025-09-30 18:53:56 +0300

    Create Apply Operations to an Array.py

---
+++ Apply Operations to an Array.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range (len(nums)-1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        nums.sort(key = lambda x: x == 0)         
+        return nums
