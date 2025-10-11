1e0274177057d64ddd2a50bab70aaefd054a292e
zekariyas716-ux <zekariyas716@gmail.com>
2025-10-11 04:33:14 +0300

    Create Build Array from Permutation.py

---
+++ Build Array from Permutation.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        res =[]
+        for i in range(len(nums)):
+            res.append(nums[nums[i]])
+        return res    
+
+      
